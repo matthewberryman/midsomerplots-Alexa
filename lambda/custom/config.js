44e40c84ac7ef1b312079093ebb9cae7d9fd8aba
Matthew Berryman <matthew@acrossthecloud.net>
2018-10-12 15:50:51 +0800

    some rework to get it to search the archive (note: untested)

---
+++ lambda/custom/config.js
@@ -0,0 +1,19 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "tba21-content"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://tba21-api.acrossthecloud.net/"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_lzb10Xjgw",
+    APP_CLIENT_ID: "2f4knvhebo7heebm6r01qod268",
+    IDENTITY_POOL_ID: "eu-central-1:77339dad-9dd5-4f20-89e8-ca44b049332c"
+  },
+  other: {
+    BASE_CONTENT_URL: 'https://tba21-content.acrossthecloud.net/'
+  }
+};
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -14,7 +14,20 @@ const Alexa = require('ask-sdk-core');
 const i18n = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
 const https = require('https');
+const Amplify = require('aws-amplify');
+const config = require('./config');
 
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: 'tba21',
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 // 1. Handlers ===================================================================================
 
@@ -55,154 +68,68 @@ const AboutHandler = {
     },
 };
 
-const CoffeeHandler = {
+
+const ItemsHandler = {
     canHandle(handlerInput) {
         const request = handlerInput.requestEnvelope.request;
 
-        return request.type === 'IntentRequest' && request.intent.name === 'CoffeeIntent';
-    },
-    handle(handlerInput) {
-        const attributesManager = handlerInput.attributesManager;
-        const responseBuilder = handlerInput.responseBuilder;
-
-        const sessionAttributes = attributesManager.getSessionAttributes();
-        const restaurant = randomArrayElement(getRestaurantsByMeal('coffee'));
-        sessionAttributes.restaurant = restaurant.name;
-        const speechOutput = `For a great coffee shop, I recommend, ${restaurant.name}. Would you like to hear more?`;
-
-        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
-    },
-};
-
-const BreakfastHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-
-        return request.type === 'IntentRequest' && request.intent.name === 'BreakfastIntent';
-    },
-    handle(handlerInput) {
-        const attributesManager = handlerInput.attributesManager;
-        const responseBuilder = handlerInput.responseBuilder;
-
-        const sessionAttributes = attributesManager.getSessionAttributes();
-        const restaurant = randomArrayElement(getRestaurantsByMeal('breakfast'));
-        sessionAttributes.restaurant = restaurant.name;
-        const speechOutput = `For breakfast, try this, ${restaurant.name}. Would you like to hear more?`;
-
-        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
-    },
-};
-
-const LunchHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-
-        return request.type === 'IntentRequest' && request.intent.name === 'LunchIntent';
-    },
-    handle(handlerInput) {
-        const attributesManager = handlerInput.attributesManager;
-        const responseBuilder = handlerInput.responseBuilder;
-
-        const sessionAttributes = attributesManager.getSessionAttributes();
-        const restaurant = randomArrayElement(getRestaurantsByMeal('lunch'));
-        sessionAttributes.restaurant = restaurant.name;
-        const speechOutput = `Lunch time! Here is a good spot. ${restaurant.name}. Would you like to hear more?`;
-
-        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
-    },
-};
-
-const DinnerHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-
-        return request.type === 'IntentRequest' && request.intent.name === 'DinnerIntent';
-    },
-    handle(handlerInput) {
-        const attributesManager = handlerInput.attributesManager;
-        const responseBuilder = handlerInput.responseBuilder;
-
-        const sessionAttributes = attributesManager.getSessionAttributes();
-        const restaurant = randomArrayElement(getRestaurantsByMeal('dinner'));
-        sessionAttributes.restaurant = restaurant.name;
-        const speechOutput = `Enjoy dinner at, ${restaurant.name}. Would you like to hear more?`;
-
-        return responseBuilder.speak(speechOutput).reprompt(speechOutput).getResponse();
-    },
-};
-
-const YesHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-
-        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.YesIntent';
-    },
-    handle(handlerInput) {
-        const attributesManager = handlerInput.attributesManager;
-        const responseBuilder = handlerInput.responseBuilder;
-
-        const sessionAttributes = attributesManager.getSessionAttributes();
-        const restaurantName = sessionAttributes.restaurant;
-        const restaurantDetails = getRestaurantByName(restaurantName);
-        const speechOutput = `${restaurantDetails.name
-        } is located at ${restaurantDetails.address
-        }, the phone number is ${restaurantDetails.phone
-        }, and the description is, ${restaurantDetails.description
-        }  I have sent these details to the Alexa App on your phone.  Enjoy your meal!
-        <say-as interpret-as="interjection">bon appetit</say-as>`;
-
-        const card = `${restaurantDetails.name}\n${restaurantDetails.address}\n$
-        {data.city}, ${data.state} ${data.postcode}\nphone: ${restaurantDetails.phone}\n`;
-
-        return responseBuilder
-            .speak(speechOutput)
-            .withSimpleCard(SKILL_NAME, card)
-            .getResponse();
-    },
-};
-
-const AttractionHandler = {
-    canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-
-        return request.type === 'IntentRequest' && request.intent.name === 'AttractionIntent';
+        return request.type === 'IntentRequest' && request.intent.name === 'ItemIntent';
     },
     handle(handlerInput) {
         const request = handlerInput.requestEnvelope.request;
         const responseBuilder = handlerInput.responseBuilder;
 
-        let distance = 200;
-        if (request.intent.slots.distance.value && request.intent.slots.distance.value !== "?") {
-            distance = request.intent.slots.distance.value;
+        let searchTerm = '';
+        if (request.intent.slots.keyword.value && request.intent.slots.keyword.value !== "?") {
+            searchTerm = request.intent.slots.keyword.value;
         }
 
-        const attraction = randomArrayElement(getAttractionsByDistance(distance));
+        const result = searchItems(searchTerm);
 
-        const speechOutput = `Try ${
-            attraction.name}, which is ${
-            attraction.distance === '0' ? 'right downtown. ' : `${attraction.distance} miles away. Have fun! `
-        }${attraction.description}`;
+        let speechOutput = '';
+
+        if (result.Items.length===0) {
+          speechOutput = 'Sorry, no matching items found.';
+        } else {
+          speechOutput = 'I found these matching items. ';
+          for (let idx = 0; idx < result.Items.length; idx++) {
+            speechOutput += `Item ${idx+1} is ${result.Items[idx].description} located in the ${result.Items[idx].ocean} ocean.`;
+          }
+
+        }
 
         return responseBuilder.speak(speechOutput).getResponse();
     },
 };
 
-const GoOutHandler = {
+const PeopleHandler = {
     canHandle(handlerInput) {
         const request = handlerInput.requestEnvelope.request;
 
-        return request.type === 'IntentRequest' && request.intent.name === 'GoOutIntent';
+        return request.type === 'IntentRequest' && request.intent.name === 'ItemIntent';
     },
     handle(handlerInput) {
-        return new Promise((resolve) => {
-            getWeather((localTime, currentTemp, currentCondition) => {
-                const speechOutput = `It is ${localTime
-                } and the weather in ${data.city
-                } is ${
-                    currentTemp} and ${currentCondition}`;
-                resolve(handlerInput.responseBuilder.speak(speechOutput).getResponse());
-            });
-        });
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let searchTerm = '';
+        if (request.intent.slots.keyword.value && request.intent.slots.keyword.value !== "?") {
+            searchTerm = request.intent.slots.keyword.value;
+        }
+
+        const result = searchItems(searchTerm);
+
+        let speechOutput = '';
+
+        if (result.Items.length===0) {
+          speechOutput = 'Sorry, no matching people found.';
+        } else {
+          speechOutput = 'I found these matching people. ';
+          for (let idx = 0; idx < result.Items.length; idx++) {
+            speechOutput += `Person ${idx+1} is ${result.Items[idx].name} and has biography as follows. ${result.Items[idx].biography}.`;
+          }
+        }
+        return responseBuilder.speak(speechOutput).getResponse();
     },
 };
 
@@ -312,163 +239,64 @@ const FallbackHandler = {
 const languageStrings = {
     en: {
         translation: {
-            WELCOME: 'Welcome to Gloucester Guide!',
-            HELP: 'Say about, to hear more about the city, or say coffee, breakfast, lunch, or dinner, to hear local restaurant suggestions, or say recommend an attraction, or say, go outside. ',
-            ABOUT: 'Gloucester Massachusetts is a city on the Atlantic Ocean.  A popular summer beach destination, Gloucester has a rich history of fishing and ship building.',
+            WELCOME: 'Welcome to the Ocean Archive!',
+            HELP: 'Say about, to hear more about the archive, or search items for, followed by a keyword to search items, or similarly to search people, say search people for, followed by a keyword.',
+            ABOUT: 'The Ocean Archive is an archive of ocean artefacts developed by the TBA21 Academy in collaboration with USER Group and Across the Cloud.',
             STOP: 'Okay, see you next time!',
         },
-    },
-    // , 'de-DE': { 'translation' : { 'TITLE'   : "Local Helfer etc." } }
-};
-const data = {
-    city: 'Gloucester',
-    state: 'MA',
-    postcode: '01930',
-    restaurants: [
-        {
-            name: "Zeke's Place",
-            address: '66 East Main Street',
-            phone: '978-283-0474',
-            meals: 'breakfast, lunch',
-            description: 'A cozy and popular spot for breakfast.  Try the blueberry french toast!',
-        },
-        {
-            name: 'Morning Glory Coffee Shop',
-            address: '25 Western Avenue',
-            phone: '978-281-1851',
-            meals: 'coffee, breakfast, lunch',
-            description: 'A homestyle diner located just across the street from the harbor sea wall.',
-        },
-        {
-            name: 'Sugar Magnolias',
-            address: '112 Main Street',
-            phone: '978-281-5310',
-            meals: 'breakfast, lunch',
-            description: 'A quaint eatery, popular for weekend brunch.  Try the carrot cake pancakes.',
-        },
-        {
-            name: 'Seaport Grille',
-            address: '6 Rowe Square',
-            phone: '978-282-9799',
-            meals: 'lunch, dinner',
-            description: 'Serving seafood, steak and casual fare.  Enjoy harbor views on the deck.',
-        },
-        {
-            name: 'Latitude 43',
-            address: '25 Rogers Street',
-            phone: '978-281-0223',
-            meals: 'lunch, dinner',
-            description: 'Features artsy decor and sushi specials.  Live music evenings at the adjoining Minglewood Tavern.',
-        },
-        {
-            name: "George's Coffee Shop",
-            address: '178 Washington Street',
-            phone: '978-281-1910',
-            meals: 'coffee, breakfast, lunch',
-            description: 'A highly rated local diner with generously sized plates.',
-        },
-
-    ],
-    attractions: [
-        {
-            name: 'Whale Watching',
-            description: 'Gloucester has tour boats that depart twice daily from Rogers street at the harbor.  Try either the 7 Seas Whale Watch, or Captain Bill and Sons Whale Watch. ',
-            distance: '0',
-        },
-        {
-            name: 'Good Harbor Beach',
-            description: 'Facing the Atlantic Ocean, Good Harbor Beach has huge expanses of soft white sand that attracts hundreds of visitors every day during the summer.',
-            distance: '2',
-        },
-        {
-            name: 'Rockport',
-            description: 'A quaint New England town, Rockport is famous for rocky beaches, seaside parks, lobster fishing boats, and several art studios.',
-            distance: '4',
-        },
-        {
-            name: 'Fenway Park',
-            description: 'Home of the Boston Red Sox, Fenway park hosts baseball games From April until October, and is open for tours. ',
-            distance: '38',
-        },
-    ],
+    }
 };
 
-const SKILL_NAME = 'Gloucester Guide';
-const FALLBACK_MESSAGE = `The ${SKILL_NAME} skill can\'t help you with that.  It can help you learn about Gloucester if you say tell me about this place. What can I help you with?`;
+const SKILL_NAME = 'ocean archive';
+const FALLBACK_MESSAGE = `The ${SKILL_NAME} can\'t help you with that.  You can ask me to search for people and items in the archive. What can I help you with?`;
 const FALLBACK_REPROMPT = 'What can I help you with?';
-
 
 
 // 3. Helper Functions ==========================================================================
 
-
-const myAPI = {
-    host: 'query.yahooapis.com',
-    port: 443,
-    path: `/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(data.city)}%2C%20${data.state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`,
-    method: 'GET',
+const getItems = (searchTerm) => {
+  return API.get('tba21', 'items')
+    .then((response) => { // tslint:disable-line: no-any
+      const result = response.Items.filter(
+        item => {
+          if (item.ocean.toLowerCase().includes(term)) {
+            return true;
+          } else if (item.description.toLowerCase().includes(term)) {
+            return true;
+          } else if (item.tags.toString().toLowerCase().includes(term)) {
+            return true;
+          } else if (item.people.map(person => person.personName + person.roles.toString()).toString().toLowerCase().includes(term)) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      );
+      return result;
+    }).catch((e) => {
+      console.log(e);
+    });
 };
 
-function getRestaurantsByMeal(mealType) {
-    const list = [];
-    for (let i = 0; i < data.restaurants.length; i += 1) {
-        if (data.restaurants[i].meals.search(mealType) > -1) {
-            list.push(data.restaurants[i]);
+const getPeople = (searchTerm) => {
+  API.get('tba21', 'items')
+    .then((response) => { // tslint:disable-line: no-any
+      const result = response.Items.filter(
+        item => {
+          if (item.name.toLowerCase().includes(term.toLowerCase())) {
+            return true;
+          } else if (item.biography.toLowerCase().includes(term.toLowerCase())) {
+            return true;
+          } else {
+            return false;
+          }
         }
-    }
-    return list;
-}
-
-function getRestaurantByName(restaurantName) {
-    let restaurant = {};
-    for (let i = 0; i < data.restaurants.length; i += 1) {
-        if (data.restaurants[i].name === restaurantName) {
-            restaurant = data.restaurants[i];
-        }
-    }
-    return restaurant;
-}
-
-function getAttractionsByDistance(maxDistance) {
-    const list = [];
-
-    for (let i = 0; i < data.attractions.length; i += 1) {
-        if (parseInt(data.attractions[i].distance, 10) <= maxDistance) {
-            list.push(data.attractions[i]);
-        }
-    }
-    return list;
-}
-
-function getWeather(callback) {
-    const req = https.request(myAPI, (res) => {
-        res.setEncoding('utf8');
-        let returnData = '';
-
-        res.on('data', (chunk) => {
-            returnData += chunk;
-        });
-        res.on('end', () => {
-            const channelObj = JSON.parse(returnData).query.results.channel;
-
-            let localTime = channelObj.lastBuildDate.toString();
-            localTime = localTime.substring(17, 25).trim();
-
-            const currentTemp = channelObj.item.condition.temp;
-
-            const currentCondition = channelObj.item.condition.text;
-
-            callback(localTime, currentTemp, currentCondition);
-        });
+      );
+      return result;
+    }).catch((e) => {
+      console.log(e);
     });
-    req.end();
-}
-
-function randomArrayElement(array) {
-    let i = 0;
-    i = Math.floor(Math.random() * array.length);
-    return (array[i]);
-}
+};
 
 const LocalizationInterceptor = {
     process(handlerInput) {
@@ -493,13 +321,8 @@ exports.handler = skillBuilder
     .addRequestHandlers(
         LaunchHandler,
         AboutHandler,
-        CoffeeHandler,
-        BreakfastHandler,
-        LunchHandler,
-        DinnerHandler,
-        YesHandler,
-        AttractionHandler,
-        GoOutHandler,
+        ItemsHandler,
+        PeopleHandler,
         HelpHandler,
         StopHandler,
         FallbackHandler,
