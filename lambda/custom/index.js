80b952397d2d16b12df4bae5d982b239e45a1aa9
Matthew Berryman <matthew@acrossthecloud.net>
2018-10-12 17:41:50 +0800

    more fixes

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -227,7 +227,7 @@ const languageStrings = {
     en: {
         translation: {
             WELCOME: 'Welcome to the Ocean Archive!',
-            HELP: 'Say about, or say Alexa ask ocean archive to search people for keyword, or say Alexa ask ocean archive to search items for keyword.',
+            HELP: 'Say about, or say Alexa ask archive to search people for keyword, or say Alexa ask ocean archive to search items for keyword.',
             ABOUT: 'The Ocean Archive is an archive of ocean artefacts developed by the TBA21 Academy in collaboration with USER Group and Across the Cloud.',
             STOP: 'Okay, see you next time!',
         },
